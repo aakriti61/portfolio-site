54648ea6a3fdd35f354f8e81498e39800452fcc7
aakriti61 <aakriti200409@gmail.com>
2026-08-12 19:49:42 +0545

    modified footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="site-footer">
-            <p>&copy; 2026 Aakritit</p>
+            <p>&copy; 2026 Aakriti</p>
         </footer>
     );
 }
